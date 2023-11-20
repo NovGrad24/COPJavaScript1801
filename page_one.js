10a58763e730fbd8829a021606be3758f103b2f0
NovGrad24 <qrentella.adams1@smail.rasmussen.edu>
2023-11-20 08:12:33 -0500

    page 1
    First JavaScript

---
+++ page_one.js
@@ -0,0 +1,10 @@
+var myheading= "<h1>You have entered the world of WeWannaBe!</h1>",
+linktag= "<a href= \"http://sotd.us/qrentellaadams1/pageone.html">Link Here</a>",
+begineffect = "<em>",
+endeffect= "</em>",
+beginpara= "<p>",
+endpara= "</p>";
+
+document.write(myheading);
+document.write(begineffect + sometext + endeffect);
+document.write(beginpara + linktag + endpara);
